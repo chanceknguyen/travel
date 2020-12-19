856e428331df6f35945dec0b3ee0aaf2240adbd4
Chance Nguyen <chanceknguyen@gmail.com>
2020-12-18 22:46:21 -0600

    parse forecast and separate utilFunctions

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 require('dotenv').config();
+const utilFunctions = require ('./utilFunctions.js');
 
 const weatherAPI = process.env.WEATHER_API_KEY;
 const PORT = process.env.PORT || 3000;
@@ -11,12 +12,13 @@ app.use(express.static('./dist'));
 
 app.get('/api/:location', (req, res) => {
   const location = req.params.location;
+  let data = {};
   axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${weatherAPI}&q=${location}&days=3`)
     .then((response) => {
-      console.log(response.data.forecast);
-      res.send(response.data.forecast);
+      data.weather = utilFunctions.parseForecast(response);
+      res.send(data);
     })
-})
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
---
+++ server/utilFunctions.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+  parseForecast: (response) => {
+    let weather = {};
+    const forecast = response.data.forecast.forecastday;
+    weather.firstDay = {
+      date: forecast[0].date,
+      mintemp_f: forecast[0].day.mintemp_f,
+      maxtemp_f: forecast[0].day.maxtemp_f,
+      avgtemp_f: forecast[0].day.avgtemp_f,
+      condition: {
+        text: forecast[0].day.condition.text,
+        icon: forecast[0].day.condition.icon
+      }
+    };
+    weather.secondDay = {
+      date: forecast[1].date,
+      mintemp_f: forecast[1].day.mintemp_f,
+      maxtemp_f: forecast[1].day.maxtemp_f,
+      avgtemp_f: forecast[1].day.avgtemp_f,
+      condition: {
+        text: forecast[1].day.condition.text,
+        icon: forecast[1].day.condition.icon
+      }
+    };
+    weather.thirdDay = {
+      date: forecast[2].date,
+      mintemp_f: forecast[2].day.mintemp_f,
+      maxtemp_f: forecast[2].day.maxtemp_f,
+      avgtemp_f: forecast[2].day.avgtemp_f,
+      condition: {
+        text: forecast[2].day.condition.text,
+        icon: forecast[2].day.condition.icon
+      }
+    };
+   return weather;
+  }
+
+}
+
+
